8cfb3374ba1eb080d954436c0415bf61dd09c559
MKutluca <mert.kutluca@accenture.com>
2021-05-13 23:59:34 +0300

    create project

---
+++ SivasSighter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SivasSighter
+//
+//  Created by mert.kutluca on 13.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
