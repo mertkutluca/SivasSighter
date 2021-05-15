04f1448cfdc541c8d2acd4603daf66bdddd3d00f
MKutluca <mert.kutluca@accenture.com>
2021-05-15 13:52:13 +0300

    divide app and scene

---
+++ SivasSighter/App/AppContainer.swift
@@ -0,0 +1,14 @@
+//
+//  AppContainer.swift
+//  SivasSighter
+//
+//  Created by mert.kutluca on 14.05.2021.
+//
+
+import Foundation
+
+let app = AppContainer()
+
+final class AppContainer {
+    let router = AppRouter()
+}
---
+++ SivasSighter/App/AppRouter.swift
@@ -0,0 +1,23 @@
+//
+//  AppRouter.swift
+//  SivasSighter
+//
+//  Created by mert.kutluca on 14.05.2021.
+//
+
+import UIKit
+
+final class AppRouter {
+    
+    var mainWindow: UIWindow?
+    
+    func start(window: UIWindow) {
+        mainWindow = window
+        // Has to crash if window not exist
+        let vc = UIViewController()
+        vc.view.backgroundColor = .red
+        let nc = UINavigationController(rootViewController: vc)
+        mainWindow!.rootViewController = nc
+        mainWindow!.makeKeyAndVisible()
+    }
+}
